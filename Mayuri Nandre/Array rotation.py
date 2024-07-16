b05bd8667fb73e0706e86997b27a53a6cf6acc97
unknown <mayurinandre@gmail.com>
2024-07-16 12:39:12 +0530

    Add new folder for Mayuri

--- Mayuri Nandre/Array rotation.py
+++ Mayuri Nandre/Array rotation.py
@@ -1,24 +1,15 @@
 def rotate_array(nums, k):
-   
     n = len(nums)
     k %= n 
-    
     def reverse(nums, start, end):
         while start < end:
-            nums[start], nums[end] = nums[end], nums[start]
-            start += 1
-            end -= 1
-    
-   
+         nums[start], nums[end] = nums[end], nums[start]
+        start += 1
+        end -= 1
     reverse(nums, 0, n - 1)
-   
     reverse(nums, 0, k - 1)
-   
     reverse(nums, k, n - 1)
-    
     return nums
-
-# Example 
 array = [1, 2, 3, 4, 5, 6, 7]
 k = 3
 print(rotate_array(array, k))  # Output: [5, 6, 7, 1, 2, 3, 4]
