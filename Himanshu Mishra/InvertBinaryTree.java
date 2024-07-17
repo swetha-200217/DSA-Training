4737876fdaa958bef72402dab900e5cd8228cf16
himanshu6387 <128816495+himanshu6387@users.noreply.github.com>
2024-07-17 10:22:30 +0530

    Invert Binary Tree

---
+++ Himanshu Mishra/InvertBinaryTree.java
@@ -0,0 +1,47 @@
+public class InvertBinaryTree {
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        // Swap the left and right children
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+    public static void printTree(TreeNode root) {
+        if (root != null) {
+            printTree(root.left);
+            System.out.print(root.val + " ");
+            printTree(root.right);
+        }
+    }
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        System.out.println("Original Tree:");
+        printTree(root);
+        System.out.println();
+        invertTree(root);
+        System.out.println("Inverted Tree:");
+        printTree(root);
+    }
+}
