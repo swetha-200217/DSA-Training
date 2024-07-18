01b6747ac1203dc976e095168895cd6052d3c523
Himanshu Mishra <128816495+himanshu6387@users.noreply.github.com>
2024-07-18 10:33:53 +0530

    ClimbStairs.java

--- Himanshu Mishra/ClimbStairs.java
+++ Himanshu Mishra/ClimbStairs.java
@@ -1,21 +1,23 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
 public class ClimbStairs {
     private static Map<Integer, Integer> memo = new HashMap<>();
     public static int climbStairs(int n) {
-        if (n == 0) return 1;  
-        if (n == 1) return 1; 
-        
+        if (n == 0) return 1;
+        if (n == 1) return 1;
         if (memo.containsKey(n)) {
             return memo.get(n);
         }
-       int result = climbStairs(n - 1) + climbStairs(n - 2);
+        int result = climbStairs(n - 1) + climbStairs(n - 2);
         memo.put(n, result);
-
         return result;
     }
     public static void main(String[] args) {
-        int n = 5; // Example number of steps
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of steps:");
+        int n = scanner.nextInt();
         System.out.println("Number of ways to climb " + n + " steps: " + climbStairs(n));
     }
 }
