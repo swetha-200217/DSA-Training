072af201b1f3e8cff596d099ea131585f5ecfa2b
Himanshu Mishra <128816495+himanshu6387@users.noreply.github.com>
2024-07-18 10:32:06 +0530

    Duplicate.java

--- Himanshu Mishra/Duplicate.java
+++ Himanshu Mishra/Duplicate.java
@@ -1,16 +1,24 @@
-public class Duplicate{
-    public static boolean containsDuplicate(int[] nums){
-        for(int i=0; i<nums.length;i++){
-            for(int j=i+1; j<nums.length;j++){
-                if(nums[i] == nums[j]){
-                    return true;  //Duplicate found
+import java.util.Scanner;
+public class Duplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;  // Duplicate found
                 }
             }
         }
-        return false;  //No Duplicate Found
+        return false;  // No Duplicate Found
     }
     public static void main(String[] args) {
-        int arr [] = {1,2,4,6,8,2,7,4,5};
-        System.out.println(containsDuplicate(arr));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Contains Duplicate: " + containsDuplicate(arr));
     }
 }
